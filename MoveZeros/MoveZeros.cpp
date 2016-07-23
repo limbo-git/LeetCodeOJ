eb5037647c40765c7c05a6687cb6f8fc08678170
limbo <479370251@qq.com>
2016-07-23 16:14:19 +0800

    add a recommand Solution

--- MoveZeros/MoveZeros.cpp
+++ MoveZeros/MoveZeros.cpp
@@ -29,6 +29,24 @@ public:
 		}
 
 	}
+
+	// recommand funcation
+	void moveZeros(vector<int>& nums)
+	{
+		size_t totalZeroes = 0;
+		for (size_t i = 0; i != nums.size(); i++) 
+		{
+			// calc total Zeroes numbers
+			if (nums[i] == 0)
+				totalZeroes++;
+			else
+				nums[i - totalZeroes] = nums[i]; // modify non-zero number put head of the vector
+		}
+		for (size_t i = nums.size() - totalZeroes; i != nums.size(); i++)
+		{
+			nums[i] = 0; // put 0 into tail of the vector;
+		}
+	}
 };
 
 int main()
